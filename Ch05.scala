839d379f3f9f44d912b07dbe70f7b27acae1d308
Sam <samuel.roeca@gmail.com>
2015-06-29 14:33:42 -0400

    started ch 5

---
+++ Ch05.scala
@@ -0,0 +1,82 @@
+import annotation.tailrec
+
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h,t) => Some(h())
+  }
+
+  // Problem 5.1
+  def toListRecursive: List[A] = this match {
+    case Cons(h,t) = h() :: t().toList
+    case _ => List()
+  }
+
+  def toList: List[A] = {
+    @tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+
+  def toListFast: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @tailrec
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h,t) => 
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h,t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h,t) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h()) => cons(h(), t() takeWhile p)
+    case _ => empty
+  }
+
+  def exists(p: A => Boolean): Boolean = this match {
+    case Cons(h,t) => p(h()) || t().exists(p)
+    case _ => false
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = 
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists2(p: A => Boolean): Boolean = 
+    foldRight(false)((a,b) => p(a) || b)
+
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = 
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
